a68faf5a2d489289bf3ab6e365fda369289f98b0
MacOS <mac@192.168.102.9>
2024-05-25 10:03:53 +0700

    .

--- deploy/Jenkinsfile.pipeline.groovy
+++ deploy/Jenkinsfile.pipeline.groovy
@@ -18,7 +18,11 @@ pipeline {
         }
         stage('build & push') {
             steps {
-                echo 'build & push' 
+                // This step should not normally be used in your script. Consult the inline help for details.
+                withDockerRegistry(credentialsId: 'docker-hub', url: 'https://index.docker.io/v1/') {
+                    sh 'docker build -t vietanhd14cn7/jenkins-practice:v1 -f deploy/Dockerfile .'
+                    sh 'docker push vietanhd14cn7/jenkins-practice:v1'
+                }
             }
         }
         stage('deployment') {
